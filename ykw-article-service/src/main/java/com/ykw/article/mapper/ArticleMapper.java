3c1b127835b3cfa46bc40a3e6ec565b2fe6c0720
Sriram <sriram.simhadri@gmail.com>
2026-05-02 23:55:59 +0400

    Fix nullpointer

--- ykw-article-service/src/main/java/com/ykw/article/mapper/ArticleMapper.java
+++ ykw-article-service/src/main/java/com/ykw/article/mapper/ArticleMapper.java
@@ -2,6 +2,8 @@ package com.ykw.article.mapper;
 
 import com.ykw.article.dto.ArticleResponse;
 import com.ykw.article.model.article.Article;
+import com.ykw.common.logging.LogEvent;
+import com.ykw.common.logging.LogUtil;
 import org.mapstruct.Mapper;
 
 import java.time.Instant;
@@ -14,6 +16,19 @@ public interface ArticleMapper {
     ArticleResponse toResponse(Article article);
 
     ArticleResponse toResponse(com.ykw.proto.Article article);
+
+    default Instant mapStringToInstant(String source) {
+        if (source == null || source.trim().isEmpty()) {
+            return null;
+        }
+        try {
+            return Instant.parse(source);
+        } catch (Exception e) {
+            LogUtil.error(LogEvent.create("EMPTY_DATE_CONVERSION_ERROR")
+                    .add("ERROR", e.getMessage()));
+            return null;
+        }
+    }
 
     default OffsetDateTime map(Instant instant) {
         if (instant == null) {
